8e52912ffec570bc405abc70579b2eb4a03e8c43
rana asad jamil <ranaasadjamil1122@gmail.com>
2023-05-01 15:30:17 +0500

    istall auth
    run npm watch

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,18 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.master.app');
 });
-Route::get('/test', function () {
     
-//    $data = DB::table('supplier')->get();
-$data = DB::table('product')
-->Leftjoin('category','category.id','product.category_id')
-->Leftjoin('supplier','supplier.id','product.supplier_id')
-->select('supplier.name as supplier_name','supplier.phone as supplier_phone','supplier.address as supplier_address',
-'product.name as product_name','product.price as product_price','product.description as product_description','product.supplier_id as P_supplier_id',
-'product.category_id as p_category_id',
-'category.name as c_name','category.type as c_type',
-)
-->get();
- dd($data);
+Auth::routes();
 
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
